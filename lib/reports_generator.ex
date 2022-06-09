a0948e28468edf196e722c3d06cfe83d0dbc5a57
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-06-09 15:21:58 -0300

    chore: returning the higher cost

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -7,6 +7,8 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_key, value} -> value end)
+
   defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
