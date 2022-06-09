eeb9f3ce18abe0a5dbb55ddcb46f0cde352ec0a5
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-06-09 10:17:36 -0300

    chore: install and configure credo

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,17 +1,4 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
   def hello do
     :world
   end
