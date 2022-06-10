f7e0c94e15fa3f16ae79acadc2f4cd227dfe18e0
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-06-10 10:30:12 -0300

    chore: implements first parallel version

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -20,23 +20,45 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
   def fetch_higher_cost(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
 
   def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
 
-  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users}) do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
-    %{report | "users" => users, "foods" => foods}
+    build_report(foods, users)
+  end
+
+  defp sum_reports(%{"foods" => foods_1, "users" => users_1}, %{
+         "foods" => foods_2,
+         "users" => users_2
+       }) do
+    foods = merge_maps(foods_1, foods_2)
+    users = merge_maps(users_1, users_2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map_1, map_2) do
+    Map.merge(map_1, map_2, fn _key, value_1, value_2 -> value_1 + value_2 end)
   end
 
   defp report_acc do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
