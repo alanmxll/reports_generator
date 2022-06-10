2a309fdebf339ea33afbb9120d55c25c52a1fe87
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-06-10 08:40:47 -0300

    chore: implements reports generator tests

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -1,8 +1,99 @@
 defmodule ReportsGeneratorTest do
   use ExUnit.Case
-  doctest ReportsGenerator
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  describe "build/1" do
+    test "should build the report" do
+      filename = "report_test.csv"
+
+      response = ReportsGenerator.build(filename)
+
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hambúrguer" => 2,
+          "pastel" => 0,
+          "pizza" => 2,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 45,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 31,
+          "30" => 0,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "fetch_higher_cost/2" do
+    test "should returns the user who spent the most when the option is 'users'" do
+      filename = "report_test.csv"
+
+      response =
+        filename
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("users")
+
+      expected_response = {:ok, {"5", 49}}
+
+      assert response == expected_response
+    end
+
+    test "should returns the most consumed food when the option is 'foods'" do
+      filename = "report_test.csv"
+
+      response =
+        filename
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("foods")
+
+      expected_response = {:ok, {"esfirra", 3}}
+
+      assert response == expected_response
+    end
+
+    test "should returns an error when an invalid option is given" do
+      filename = "report_test.csv"
+
+      response =
+        filename
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("foo")
+
+      expected_response = {:error, "Invalid option"}
+
+      assert response == expected_response
+    end
   end
 end
